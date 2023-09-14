5ce826f6749a5ed7c94f3e910389ff893372b01b
vanshikasharma <vanshikasharma@localhost>
2023-09-14 06:42:07 +0530

    first commit

---
+++ demo1/src/main/java/com/statetstreet/demo1/Demo1Application.java
@@ -0,0 +1,23 @@
+package com.statetstreet.demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Demo1Application {
+
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats in Azure";
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Demo1Application.class, args);
+	}
+
+}
